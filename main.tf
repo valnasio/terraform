25fb0e9f4447f950e8af9344d7477ec54489029b
valnasio <valnasio1@gmail.com>
2025-02-16 15:38:23 -0300

    aula-04

--- main.tf
+++ main.tf
@@ -2,13 +2,34 @@ terraform {
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = "4.16"
+        version = "5.87.0" #versão do repo-provider
     }
   } 
 
-  required_version = ">= 1.10.5" #setar a versão do seu terraform
+  required_version = ">= 1.10.5" #versão do terraform
 }
 provider "aws" {
-  region = "us-east-1"
-  profile = "bia"
+  region = "us-east-1" # região da vpc
+  profile = "bia" # nome do perfil no IAM
+}
+# Configurando instancia ec2
+resource "aws_instance" "bia-dev-t3" {
+  ami = "ami-02f3f602d23f1659d" # ID da imagem do SO
+  instance_type = "t3.micro" # Tipo instância
+  tags = {
+    ambiente = "dev"
+    Name = "bia-dev"
+  }
+  vpc_security_group_ids = [ "sg-06440efafa77afd27"  ]
+
+    root_block_device { # configura o EBS
+    
+    delete_on_termination = false
+    encrypted = false
+    iops = 3000
+    volume_size = 20
+    volume_type = "gp3"
+              
+    }
+
 }
